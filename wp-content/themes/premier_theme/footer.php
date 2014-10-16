7b23e8d2ba78e192353ab4f17555b19ae6608976
Guiks <guillaume.delapre@gmail.com>
2014-10-16 15:29:45 +0200

    Version stable

--- wp-content/themes/premier_theme/footer.php
+++ wp-content/themes/premier_theme/footer.php
@@ -5,15 +5,12 @@
 		<br />
 			Blog propulsé par <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par <a href="http://www.fran6art.com">Fran6art</a>
 		<br />
-		<a href="feed:<?php bloginfo('rss2_url'); ?>">Articles (RSS)</a> et <a href="feed:<?php </a> et con&ccedil;u par <a href="http://www.fran6art.com">Fran6art</a>
+		<a href="feed:<?php bloginfo('rss2_url'); ?>">Articles (RSS)</a>
+		<a href="http://www.fran6art.com">Fran6art</a>
 			<br />
 			<a href="feed:<?php bloginfo('rss2_url'); ?>">Articles (RSS)</a> et <a href="feed:<?php bloginfo('comments_rss2_url');
  ?>">Commentaires (RSS)</a>. <?php echo get_num_queries(); ?>
-		requêtes. <?php <div id="footer">
-			<p>
-			Copyright &#169; <?php print(date(Y)); ?>
-			<?php bloginfo('name');
-			?>
+		requêtes. 
 			<br />
 			Blog propulsé par <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par <a href="http://www.fran6art.com">Fran6art</a>
 			<br />
--- wp-content/themes/premier_theme/functions.php
+++ wp-content/themes/premier_theme/functions.php
@@ -27,10 +27,5 @@ function lien_mot_de_passe_perdu( $formbottom ) {
 	return $formbottom;
 }
 
-//Create custom widgets
-add_action('widgets_init', 'theme_register_widgets');
 
-function theme_register_widgets() {
-	register_widget('CustomWidget');
-}
 ?>
