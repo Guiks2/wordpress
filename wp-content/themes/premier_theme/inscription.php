cecdd9ef3b9a7fb4e524fecf097382262dfe38aa
Guiks <guillaume.delapre@gmail.com>
2014-10-17 17:01:40 +0200

    Ajout du formulaire d'inscription
    Attention :
    	Javascript non opérationnel
    	Voir si il y a un moyen d'écrire la page autrement qu'en dur

---
+++ wp-content/themes/premier_theme/inscription.php
@@ -0,0 +1,12 @@
+			<?php 
+/*
+Template Name: Inscriptions
+*/
+				get_header();
+				register_user_form();
+				get_sidebar(); 
+				get_footer();
+			?>
+		</div>
+	</body>
+</html>
---
+++ wp-content/themes/premier_theme/js/functions.js
@@ -0,0 +1,48 @@
+/**
+ * @author Guillaume
+ */
+
+// JS de l'inscription
+jQuery( document ).ready( function($) {
+	$( '#register-user' ).on( 'submit', function(e) {
+
+		$( this ).find( 'input:required' ).each( function() {
+			// On vérifie si les input requis sont remplis
+			if ( $( this ).val().trim() == '' ) {
+				e.preventDefault();
+				$( this ).addClass( 'error' );
+			}
+		} );
+
+		$( this ).find( 'input[type="email"]' ).each( function() {
+			// Regex pour vérifier le champ email
+			var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+			if ( ! pattern.test( $( this ).val() ) ) {
+				e.preventDefault();
+				$( this ).addClass( 'error' );
+			}
+		} );
+
+	} );
+
+	// Toggle hidden password
+	$( '#show-password' ).on( 'change', function() {
+		if ( $( this ).is( ':checked' ) ) {
+			$( '#pass-user' ).attr( 'type', 'text' );
+		} else {
+			$( '#pass-user' ).attr( 'type', 'password' );
+		}
+	} );
+} );
+
+
+// JS du message d'inscription
+jQuery( document ).ready( function($) {
+
+	if ( $( '.message' ).length > 0 ) {
+		setTimeout( function() {
+			$( '.message' ).remove();
+		}, 6000 );
+	}
+
+} );
