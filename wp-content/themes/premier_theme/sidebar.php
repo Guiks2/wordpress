87c67c8dc905dcd343a552b328aa7854a5e240e5
ThibaultCrosnier <thibault.crosnier@gmail.com>
2014-10-17 15:36:18 +0200

    Mise en place de la sidebar

--- wp-content/themes/premier_theme/sidebar.php
+++ wp-content/themes/premier_theme/sidebar.php
@@ -1,5 +1,7 @@
 <div class="sidebar">
 	<ul>
+    	<li id="menu"><?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'menu' ) ); ?>
+
 		<li id="search"><?php include(TEMPLATEPATH . '/searchform.php'); ?></li>
 		<li id="calendar">
 			<h2>Calendrier</h2>
