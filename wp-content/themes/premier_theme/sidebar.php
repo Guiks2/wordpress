cd238d52ffe9d3fc3252581ae5fb031be8201c05
ThibaultCrosnier <thibault.crosnier@gmail.com>
2014-11-12 20:31:11 +0100

    MàJ BDD avec menu

--- wp-content/themes/premier_theme/sidebar.php
+++ wp-content/themes/premier_theme/sidebar.php
@@ -1,7 +1,6 @@
 <div class="sidebar">
 	<ul>
-    	<li id="menu"><?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'menu' ) ); ?>
-
+ 		<li id="menu"><?php wp_nav_menu( array( 'theme_location' => 'main_menu', 'menu_class' => 'menu' ) ); ?></li>
 		<li id="search"><?php include(TEMPLATEPATH . '/searchform.php'); ?></li>
 		<li id="calendar">
 			<h2>Calendrier</h2>
