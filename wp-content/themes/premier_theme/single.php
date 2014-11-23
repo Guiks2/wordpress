8d685add26ca55a798a5e95f2f3fb8547eb091f2
ThibaultCrosnier <thibault.crosnier@gmail.com>
2014-11-23 17:53:03 +0100

    Création des templates single et comments

--- wp-content/themes/premier_theme/single.php
+++ wp-content/themes/premier_theme/single.php
@@ -20,13 +20,15 @@
 							<?php the_title(); ?>
 						</a>	
 					</h2>
-					<p class="postmetadata">
-						<?php the_time('j F Y') ?> par <?php the_author() ?> | Cat&eacute;gorie: <?php the_category(', ') ?> | <?php comments_popup_link('Pas de commentaires', '1 Commentaire', '% Commentaires'); ?> <?php edit_post_link('Editer', ' &#124; ', ''); ?>
-					</p>	
+				
 					<div class="post_content">
 						<?php the_content(); ?>
 					</div>
 				</div>
+                <hr/><hr/>
+                <div class="comments-template">
+					<?php comments_template(); ?>
+                </div>
 				<?php
 					endwhile;
 				endif;
