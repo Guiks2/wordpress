ee134384f516496e3a9b8993ce0963a901f84599
Guiks <guillaume.delapre@gmail.com>
2014-11-25 18:49:31 +0100

    Correction javascript

--- wp-content/themes/premier_theme/sidebar.php
+++ wp-content/themes/premier_theme/sidebar.php
@@ -4,7 +4,13 @@
 		<li id="search"><h2>Rechercher sur le site</h2><?php include(TEMPLATEPATH . '/searchform.php'); ?></li>
 		<li id="calendar">
 			<h2>Calendrier</h2>
-			<?php echo do_shortcode('[ai1ec view="agenda"]') ?>
+			        	<script class="ai1ec-widget-placeholder" data-widget="ai1ec_agenda_widget" data-events_seek_type="events" data-events_per_page="5" data-show_subscribe_buttons="false">
+  (function(){var d=document,s=d.createElement('script'),
+  i='ai1ec-script';if(d.getElementById(i))return;s.async=1;
+  s.id=i;
+  jQuery(s).attr("src", "<?php echo get_site_url(); ?>/?ai1ec_js_widget");
+  d.getElementsByTagName('head')[0].appendChild(s);})();
+</script>
 		</li>
 	</ul>
 </div>
